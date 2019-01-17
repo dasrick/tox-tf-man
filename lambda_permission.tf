d7a77aac71e89c7ed632d69875c6766a6ebd042e
Enrico Hoffmann <dasrick@gmail.com>
2019-01-17 18:32:33 +0100

    add some depends_on for proper terraform apply

--- lambda_permission.tf
+++ lambda_permission.tf
@@ -4,11 +4,14 @@ resource "aws_lambda_permission" "s3_stash_lambda_unzip" {
   function_name = "${aws_lambda_function.man_unzip.function_name}"
   principal     = "s3.amazonaws.com"
   source_arn    = "${aws_s3_bucket.stash.arn}"
+  depends_on    = ["aws_lambda_function.man_unzip"]
 }
+
 resource "aws_lambda_permission" "s3_stash_lambda_import" {
   statement_id  = "AllowExecutionFromS3BucketToLambdaImport"
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.man_import.function_name}"
   principal     = "s3.amazonaws.com"
   source_arn    = "${aws_s3_bucket.stash.arn}"
+  depends_on    = ["aws_lambda_function.man_import"]
 }
--- s3.tf
+++ s3.tf
@@ -1,18 +1,22 @@
 locals {
   bucket_name_storage = "${local.name_prefix}-stash"
 }
+
 // STASH - persist whole data strtucture ===============================================================================
 resource "aws_s3_bucket" "stash" {
   bucket        = "${local.bucket_name_storage}"
   acl           = "private"
   region        = "${var.region}"
   force_destroy = true
+
   versioning {
     enabled = false
   }
+
   lifecycle {
     prevent_destroy = false
   }
+
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -20,13 +24,15 @@ resource "aws_s3_bucket" "stash" {
       }
     }
   }
-  tags          = "${merge(
+
+  tags = "${merge(
       local.common_tags,
       map(
         "Name", local.bucket_name_storage
       )
     )}"
 }
+
 resource "aws_s3_bucket_object" "stash_readme_incoming" {
   depends_on   = ["aws_s3_bucket.stash"]
   bucket       = "${aws_s3_bucket.stash.bucket}"
@@ -35,6 +41,7 @@ resource "aws_s3_bucket_object" "stash_readme_incoming" {
   content_type = "text/markdown"
   etag         = "${md5(file("docs/S3_IMPORT_README.md"))}"
 }
+
 resource "aws_s3_bucket_object" "stash_readme_uncompressed" {
   depends_on   = ["aws_s3_bucket.stash"]
   bucket       = "${aws_s3_bucket.stash.bucket}"
@@ -43,9 +50,16 @@ resource "aws_s3_bucket_object" "stash_readme_uncompressed" {
   content_type = "text/markdown"
   etag         = "${md5(file("docs/S3_UNCOMPRESSED_README.md"))}"
 }
+
 resource "aws_s3_bucket_notification" "stash_notification_incoming_unzip" {
-  depends_on = ["aws_s3_bucket.stash"]
-  bucket     = "${aws_s3_bucket.stash.id}"
+  depends_on = [
+    "aws_s3_bucket.stash",
+    "aws_s3_bucket_object.stash_readme_incoming",
+    "aws_lambda_function.man_unzip",
+  ]
+
+  bucket = "${aws_s3_bucket.stash.id}"
+
   lambda_function {
     lambda_function_arn = "${aws_lambda_function.man_unzip.arn}"
     events              = ["s3:ObjectCreated:*"]
@@ -53,9 +67,16 @@ resource "aws_s3_bucket_notification" "stash_notification_incoming_unzip" {
     filter_suffix       = ".zip"
   }
 }
+
 resource "aws_s3_bucket_notification" "stash_notification_uncompressed_import" {
-  depends_on = ["aws_s3_bucket.stash"]
-  bucket     = "${aws_s3_bucket.stash.id}"
+  depends_on = [
+    "aws_s3_bucket.stash",
+    "aws_s3_bucket_object.stash_readme_uncompressed",
+    "aws_lambda_function.man_import",
+  ]
+
+  bucket = "${aws_s3_bucket.stash.id}"
+
   lambda_function {
     lambda_function_arn = "${aws_lambda_function.man_import.arn}"
     events              = ["s3:ObjectCreated:*"]
